5497869e0d1311428102fa029fa6e6f2e159f5cd
Tor Morten Jensen <tormorten@tormorten.no>
2018-03-14 07:57:11 +0000

    Apply fixes from StyleCI

--- src/Event.php
+++ src/Event.php
@@ -4,9 +4,9 @@ namespace TorMorten\Eventy;
 
 abstract class Event
 {
-
     /**
-     * Holds the event listeners
+     * Holds the event listeners.
+     *
      * @var array
      */
     protected $listeners = null;
@@ -17,26 +17,28 @@ abstract class Event
     }
 
     /**
-     * Adds a listener
-     * @param string  $hook      Hook name
-     * @param mixed   $callback  Function to execute
-     * @param integer $priority  Priority of the action
-     * @param integer $arguments Number of arguments to accept
+     * Adds a listener.
+     *
+     * @param string $hook      Hook name
+     * @param mixed  $callback  Function to execute
+     * @param int    $priority  Priority of the action
+     * @param int    $arguments Number of arguments to accept
      */
     public function listen($hook, $callback, $priority = 20, $arguments = 1)
     {
         $this->listeners->push([
-            'hook' => $hook,
-            'callback' => $callback,
-            'priority' => $priority,
-            'arguments' => $arguments
+            'hook'      => $hook,
+            'callback'  => $callback,
+            'priority'  => $priority,
+            'arguments' => $arguments,
         ]);
 
         return $this;
     }
 
     /**
-     * Gets a sorted list of all listeners
+     * Gets a sorted list of all listeners.
+     *
      * @return array
      */
     public function getListeners()
@@ -51,15 +53,18 @@ abstract class Event
     }
 
     /**
-     * Gets the function
-     * @param  mixed $callback Callback
-     * @return mixed           A closure, an array if "class@method" or a string if "function_name"
+     * Gets the function.
+     *
+     * @param mixed $callback Callback
+     *
+     * @return mixed A closure, an array if "class@method" or a string if "function_name"
      */
     protected function getFunction($callback)
     {
         if (is_string($callback) && strpos($callback, '@')) {
             $callback = explode('@', $callback);
-            return array(app('\\' . $callback[0]), $callback[1]);
+
+            return [app('\\'.$callback[0]), $callback[1]];
         } elseif (is_callable($callback)) {
             return $callback;
         } else {
@@ -68,9 +73,10 @@ abstract class Event
     }
 
     /**
-     * Fires a new action
-     * @param  string $action Name of action
-     * @param  array  $args   Arguments passed to the action
+     * Fires a new action.
+     *
+     * @param string $action Name of action
+     * @param array  $args   Arguments passed to the action
      */
     abstract public function fire($action, $args);
 }
--- tests/Unit/FilterTest.php
+++ tests/Unit/FilterTest.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 namespace EventyTests\Unit;
 
 use EventyTests\DummyClass;
-use TorMorten\Eventy\Events;
 use PHPUnit\Framework\TestCase;
+use TorMorten\Eventy\Events;
 
 class FilterTest extends TestCase
 {
     public function setUp()
     {
-        $this->events = new Events;
+        $this->events = new Events();
     }
 
     /**
@@ -19,7 +19,7 @@ class FilterTest extends TestCase
     public function it_can_hook_a_callable()
     {
         $this->events->addFilter('my_awesome_filter', function ($value) {
-            return $value . ' Filtered';
+            return $value.' Filtered';
         });
         $this->assertEquals($this->events->filter('my_awesome_filter', 'Value Was'), 'Value Was Filtered');
     }
@@ -32,7 +32,7 @@ class FilterTest extends TestCase
         $class = new class('DummyClass') {
             public function filter($value)
             {
-                return $value . ' Filtered';
+                return $value.' Filtered';
             }
         };
         $this->events->addFilter('my_amazing_filter', [$class, 'filter']);
@@ -46,11 +46,11 @@ class FilterTest extends TestCase
     public function a_hook_fires_even_if_there_are_two_listeners_with_the_same_priority()
     {
         $this->events->addFilter('my_great_filter', function ($value) {
-            return $value . ' Once';
+            return $value.' Once';
         }, 20);
 
         $this->events->addFilter('my_great_filter', function ($value) {
-            return $value . ' And Twice';
+            return $value.' And Twice';
         }, 20);
 
         $this->assertEquals($this->events->filter('my_great_filter', 'I Was Filtered'), 'I Was Filtered Once And Twice');
@@ -62,19 +62,19 @@ class FilterTest extends TestCase
     public function listeners_are_sorted_by_priority()
     {
         $this->events->addFilter('my_awesome_filter', function ($value) {
-            return $value . ' Filtered';
+            return $value.' Filtered';
         }, 20);
 
         $this->events->addFilter('my_awesome_filter', function ($value) {
-            return $value . ' Filtered';
+            return $value.' Filtered';
         }, 8);
 
         $this->events->addFilter('my_awesome_filter', function ($value) {
-            return $value . ' Filtered';
+            return $value.' Filtered';
         }, 12);
 
         $this->events->addFilter('my_awesome_filter', function ($value) {
-            return $value . ' Filtered';
+            return $value.' Filtered';
         }, 40);
 
         $this->assertEquals($this->events->getFilter()->getListeners()->values()[0]['priority'], 8);

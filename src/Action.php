698914c9bdbc9bf9eb45ec6dcc5cdbc1e250c516
Tor Morten Jensen <tormorten@tormorten.no>
2020-03-04 14:24:39 +0100

    Fixed null parameters

--- src/Action.php
+++ src/Action.php
@@ -8,7 +8,7 @@ class Action extends Event
      * Filters a value.
      *
      * @param string $action Name of action
-     * @param array  $args   Arguments passed to the filter
+     * @param array $args Arguments passed to the filter
      *
      * @return string Always returns the value
      */
@@ -18,9 +18,8 @@ class Action extends Event
             $this->getListeners()->where('hook', $action)->each(function ($listener) use ($action, $args) {
                 $parameters = [];
                 for ($i = 0; $i < $listener['arguments']; $i++) {
-                    if (isset($args[$i])) {
-                        $parameters[] = $args[$i];
-                    }
+                    $value = $args[$i] ?? null;
+                    $parameters[] = $value;
                 }
                 call_user_func_array($this->getFunction($listener['callback']), $parameters);
             });
